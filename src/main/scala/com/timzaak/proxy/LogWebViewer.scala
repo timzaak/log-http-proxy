27e4e6471337a30243b1370b2d8e9cbe9d6d2c5b
timzaak <zsy.evan@gmail.com>
2025-06-10 11:52:53 +0800

    format

--- src/main/scala/com/timzaak/proxy/LogWebViewer.scala
+++ src/main/scala/com/timzaak/proxy/LogWebViewer.scala
@@ -1,29 +1,26 @@
 package com.timzaak.proxy
 
-import org.apache.pekko.actor.{Actor, ActorRef, ActorSystem, PoisonPill, Props}
+import org.apache.pekko.actor.{ Actor, ActorRef, ActorSystem, PoisonPill, Props }
 import org.apache.pekko.http.scaladsl.*
-import org.apache.pekko.http.scaladsl.model.{AttributeKeys, RemoteAddress}
-import org.apache.pekko.io.Tcp.SO.KeepAlive
-import org.apache.pekko.stream.{CompletionStrategy, OverflowStrategy}
+import org.apache.pekko.stream.OverflowStrategy
 import org.apache.pekko.stream.scaladsl.*
 import sttp.capabilities.pekko.PekkoStreams
 import sttp.tapir.*
 import sttp.tapir.model.ServerRequest
 import sttp.tapir.server.pekkohttp.PekkoHttpServerInterpreter
 
-import java.net.{InetAddress, InetSocketAddress}
 import scala.concurrent.Future
 
 class LogWebViewer(using system: ActorSystem) {
 
   import system.dispatcher
 
-  val manager = system.actorOf(Props[ConnectionManager]())
+  private val manager = system.actorOf(Props[ConnectionManager]())
 
   def call(request: ServerRequest, data: String) = {
     val ip = request
       .header("X-Forwarded-For")
-      .orElse(request.header("Remote-Address").map(v =>v.take(v.lastIndexOf(':'))))
+      .orElse(request.header("Remote-Address").map(v => v.take(v.lastIndexOf(':'))))
       .getOrElse(request.connectionInfo.remote.map(_.getAddress.getHostAddress).getOrElse(""))
 
     manager ! ip -> data
@@ -36,19 +33,21 @@ class LogWebViewer(using system: ActorSystem) {
     .serverLogicSuccess { ip =>
       val flow = Flow.fromSinkAndSource(
         Sink.ignore.mapMaterializedValue(_.onComplete(_ => manager ! ip)),
-        Source.actorRef[String](
-          completionMatcher = PartialFunction.empty,
-          failureMatcher = PartialFunction.empty,
-          bufferSize = 256,
-          OverflowStrategy.dropHead,
-        ).mapMaterializedValue{actorRef =>
-          manager ! ip -> actorRef
-        }
+        Source
+          .actorRef[String](
+            completionMatcher = PartialFunction.empty,
+            failureMatcher = PartialFunction.empty,
+            bufferSize = 256,
+            OverflowStrategy.dropHead,
+          )
+          .mapMaterializedValue { actorRef =>
+            manager ! ip -> actorRef
+          }
       )
       Future.successful(flow)
     }
 
-  def startServer(port:Int) = {
+  def startServer(port: Int) = {
     val bindAndCheck = Http()
       .newServerAt("0.0.0.0", port)
       .bindFlow(PekkoHttpServerInterpreter().toRoute(handler))
@@ -57,7 +56,6 @@ class LogWebViewer(using system: ActorSystem) {
 }
 
 class ConnectionManager extends Actor {
-  // todo: add filter
   private val connections = scala.collection.mutable.Map[String, ActorRef]()
   override def receive: Receive = {
     case (key: String, data: String)  => connections.get(key).foreach(ref => data.trim.split('\n').foreach(ref ! _))
